040db900bf0a12649ff6a486a77dcf9eaa920d6b
gordonmzhu <gordonmzhu@gmail.com>
2015-11-19 01:03:27 +0000

    Added code for handling emails.

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-// Twilio and Firebase info.
+// Twilio info.
 var twilioAccountSID = '';
 var twilioAuthToken = '';
 var twilioNumber = '';
+
+// Firebase info.
 var firebaseSecret = '';
 var firebaseURL = '';
+
+// // Mailgun info.
+// var mailgunApiKey = '';
+// var mailgunDomain = '';
 
 // Create references for libraries.
 var express = require('express');
 var http = require('http');
 var Firebase = require('firebase');
 var twilio = require('twilio');
+var mailgun = require('mailgun-js')({apiKey: mailgunApiKey, domain: mailgunDomain});
 
 // Express server setup.
 var router = express();
 var server = http.createServer(router);
 var twilioClient = twilio(twilioAccountSID, twilioAuthToken);
 
-// Create a reference to textMessages on Firebase
-// and authenticate the server with Firebase secret.
-var textMessagesRef = new Firebase(firebaseURL + 'textMessages');
-textMessagesRef.authWithCustomToken(firebaseSecret, function(error, authData) {
+// Authenticate Firebase.
+var firebaseRef = new Firebase(firebaseURL);
+firebaseRef.authWithCustomToken(firebaseSecret, function(error, authData) {
   if (error) {
     console.log("Firebase server authentication failed.");
   } else {
     console.log("Authenticated with Firebase secret successfully.");
   }
 });
+
+// Create a reference to textMessages.
+var textMessagesRef = firebaseRef.child('textMessages');
 
 // Listen for new objects pushed to textMessagesRef.
 textMessagesRef.on("child_added", function(snapshot) {
@@ -42,6 +51,26 @@ textMessagesRef.on("child_added", function(snapshot) {
     }
   });
 });
+
+// // Create a reference to emails.
+// var emailsRef = firebaseRef.child('emails');
+
+// // Listen for new objects pushed to emailsRef.
+// emailsRef.on("child_added", function(snapshot) {
+//   var email = snapshot.val();
+//   var emailData = {
+//     from: '<postmaster@'  + mailgunDomain + '>',
+//     to: email.email,
+//     subject: 'Welcome to Wait and Eat',
+//     text: 'Thanks for signing up for Wait and Eat!'
+//   };
+//   mailgun.messages().send(emailData, function(error, body) {
+//     console.log(body);
+//     if (error) {
+//       console.log(error);
+//     };
+//   });
+// });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
